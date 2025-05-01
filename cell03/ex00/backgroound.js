a638999c1ed02b23c8e1e73e1aad9f522b14ad45
Tutku Cakir Yilmaz <tcakir-y@f1r5s10.codam.nl>
2025-05-01 18:12:20 +0200

    finished rush project

---
+++ cell03/ex00/backgroound.js
@@ -0,0 +1,8 @@
+const random_hex_color_code = () => {
+	let n = (Math.random() * 0xfffff * 1000000).toString(16);
+	return '#' + n.slice(0, 6);
+} 
+
+random_hex_color_code();
+
+//https://www.30secondsofcode.org/js/s/random-hex-color-code/
